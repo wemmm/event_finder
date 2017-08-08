596836ce54487945d4200b2861f17e410c60b5b9
wemmm <jenny.wem@gmail.com>
2017-08-08 20:28:43 +0100

    Adds styles.css and connects interface with jQuery

--- interface.js
+++ interface.js
@@ -5,4 +5,14 @@ $(document).ready(function() {
     eventfinder.getUserCoordinates($("#xcoord").val(), $("#ycoord").val())
     $('#locationstatus').text(eventfinder.userCoordinates)
   })
+
+  $('#findevents').on('click', function() {
+    eventfinder.closestEvents();
+    $('#firstevent').text(eventfinder.returnEventList()[0])
+    $('#secondevent').text(eventfinder.returnEventList()[1])
+    $('#thirdevent').text(eventfinder.returnEventList()[2])
+    $('#fourthevent').text(eventfinder.returnEventList()[3])
+    $('#fifthevent').text(eventfinder.returnEventList()[4])
+    $('#sixthevent').text('Refresh the page for more events!')
+  })
 })
